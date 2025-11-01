2a296b2443cd0077fcff66eac34a179ad47e077e
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2025-11-01 15:55:36 +0530

    Create User, Book models and their Repository layers

---
+++ chapter-backend/src/main/java/org/varun/chapterbackend/model/User.java
@@ -0,0 +1,29 @@
+package org.varun.chapterbackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+    @ManyToMany(mappedBy = "users")
+    private List<Book> books;
+}
---
+++ chapter-backend/src/main/java/org/varun/chapterbackend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package org.varun.chapterbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.varun.chapterbackend.model.User;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
