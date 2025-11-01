b815a6962206f839e6f5d317c8ca3a1114750d50
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2025-11-01 20:20:51 +0530

    Made controller cleaner and written all the process in Service layer

--- chapter-backend/src/main/java/org/varun/chapterbackend/controller/AuthController.java
+++ chapter-backend/src/main/java/org/varun/chapterbackend/controller/AuthController.java
@@ -1,38 +1,22 @@
 package org.varun.chapterbackend.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.varun.chapterbackend.model.User;
 import org.varun.chapterbackend.model.dto.SignUpDto;
 import org.varun.chapterbackend.service.AuthService;
-import org.varun.chapterbackend.service.JwtService;
 
 @RestController
 @RequestMapping("auth")
 @AllArgsConstructor
 public class AuthController {
     private AuthService service;
-    private JwtService jwtService;
 
     @PostMapping("signup")
     public ResponseEntity<?> signUp(@RequestBody SignUpDto userDto) {
-        User existingUser = service.getUserByEmail(userDto.email());
-        if (existingUser != null) {
-            if (existingUser.isVerified()) {
-                return new ResponseEntity<>("User Already exists and Verified", HttpStatus.BAD_REQUEST);
-            } else {
-                existingUser.setVerificationCode(jwtService.generateToken(existingUser.getEmail()));
-                service.updateUser(existingUser);
-                //TODO
-                // SEND EMAIL
-                return new ResponseEntity<>("Verification Code is resent", HttpStatus.OK);
-            }
-        }
-        return new ResponseEntity<>(service.addUser(userDto, jwtService.generateToken(userDto.email())), HttpStatus.CREATED);
+        return service.signUp(userDto);
     }
 }
--- chapter-backend/src/main/java/org/varun/chapterbackend/service/AuthService.java
+++ chapter-backend/src/main/java/org/varun/chapterbackend/service/AuthService.java
@@ -1,9 +1,12 @@
 package org.varun.chapterbackend.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.varun.chapterbackend.model.User;
+import org.varun.chapterbackend.model.dto.ApiResponse;
 import org.varun.chapterbackend.model.dto.SignUpDto;
 import org.varun.chapterbackend.repository.UserRepository;
 
@@ -12,22 +15,27 @@ import org.varun.chapterbackend.repository.UserRepository;
 public class AuthService {
     private UserRepository repo;
     private BCryptPasswordEncoder encoder;
+    private JwtService jwtService;
 
-    public User addUser(SignUpDto userDto,String verificationCode) {
-        User user=new User();
+    public ResponseEntity<?> signUp(SignUpDto userDto) {
+        User existingUser=repo.findByEmail(userDto.email());
+        if(existingUser!=null){
+            if(existingUser.isVerified()){
+                return new ResponseEntity<>(new ApiResponse<>("User Already exists and Verified"),HttpStatus.BAD_GATEWAY);
+            }else{
+                existingUser.setVerificationCode(jwtService.generateToken(existingUser.getEmail()));
+                repo.save(existingUser);
+                //ToDo
+                // Send Email
+                return new ResponseEntity<>(new ApiResponse<>("User Not Verfied verification code resent"),HttpStatus.OK);
+            }
+        }
+        User user = new User();
         user.setEmail(userDto.email());
-        user.setUsername(userDto.username());
         user.setPassword(encoder.encode(userDto.password()));
-        user.setVerificationCode(verificationCode);
+        user.setUsername(userDto.username());
+        user.setVerificationCode(jwtService.generateToken(userDto.email()));
         user.setVerified(false);
-        return repo.save(user);
-    }
-
-    public User updateUser(User user){
-        return repo.save(user);
-    }
-
-    public User getUserByEmail(String email) {
-        return repo.findByEmail(email);
+        return new ResponseEntity<>(new ApiResponse<>("User Created Sucessfully. Please verify your email"),HttpStatus.CREATED);
     }
 }
