2e8d162c858cde765018397befaf55a405800029
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2025-11-01 15:24:32 +0530

    Project initialization

---
+++ chapter-backend/src/main/java/org/varun/chapterbackend/ChapterBackendApplication.java
@@ -0,0 +1,13 @@
+package org.varun.chapterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChapterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChapterBackendApplication.class, args);
+    }
+
+}
---
+++ chapter-backend/src/test/java/org/varun/chapterbackend/ChapterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.varun.chapterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChapterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
