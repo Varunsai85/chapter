ab6070504a6748c17d70adc814b2b5d58b1fa5d6
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2025-11-01 20:19:29 +0530

    Added for Api responses

---
+++ chapter-backend/src/main/java/org/varun/chapterbackend/model/dto/ApiResponse.java
@@ -0,0 +1,20 @@
+package org.varun.chapterbackend.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+public class ApiResponse<T> {
+    private String message;
+    private T data;
+
+    public ApiResponse(String message,T data){
+        this.message=message;
+        this.data=data;
+    }
+
+    public ApiResponse(String message){
+        this.message=message;
+    }
+}
