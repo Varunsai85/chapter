8de70957e02fa0eb54f29ff2db6df3b11ce55092
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2025-11-01 19:15:28 +0530

    Implemented Signup functionality

--- chapter-backend/src/main/java/org/varun/chapterbackend/controller/AuthController.java
+++ chapter-backend/src/main/java/org/varun/chapterbackend/controller/AuthController.java
@@ -8,16 +8,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.varun.chapterbackend.model.User;
+import org.varun.chapterbackend.model.dto.SignUpDto;
 import org.varun.chapterbackend.service.AuthService;
+import org.varun.chapterbackend.service.JwtService;
 
 @RestController
 @RequestMapping("auth")
 @AllArgsConstructor
 public class AuthController {
     private AuthService service;
+    private JwtService jwtService;
 
     @PostMapping("signup")
-    public ResponseEntity<User> signUp(@RequestBody User user){
-        return new ResponseEntity<>(service.addUser(user), HttpStatus.CREATED);
+    public ResponseEntity<?> signUp(@RequestBody SignUpDto userDto) {
+        User existingUser = service.getUserByEmail(userDto.email());
+        if (existingUser != null) {
+            if (existingUser.isVerified()) {
+                return new ResponseEntity<>("User Already exists and Verified", HttpStatus.BAD_REQUEST);
+            } else {
+                existingUser.setVerificationCode(jwtService.generateToken(existingUser.getEmail()));
+                service.updateUser(existingUser);
+                //TODO
+                // SEND EMAIL
+                return new ResponseEntity<>("Verification Code is resent", HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>(service.addUser(userDto, jwtService.generateToken(userDto.email())), HttpStatus.CREATED);
     }
 }
--- chapter-backend/src/main/java/org/varun/chapterbackend/service/AuthService.java
+++ chapter-backend/src/main/java/org/varun/chapterbackend/service/AuthService.java
@@ -1,4 +1,33 @@
 package org.varun.chapterbackend.service;
 
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.varun.chapterbackend.model.User;
+import org.varun.chapterbackend.model.dto.SignUpDto;
+import org.varun.chapterbackend.repository.UserRepository;
+
+@Service
+@AllArgsConstructor
 public class AuthService {
+    private UserRepository repo;
+    private BCryptPasswordEncoder encoder;
+
+    public User addUser(SignUpDto userDto,String verificationCode) {
+        User user=new User();
+        user.setEmail(userDto.email());
+        user.setUsername(userDto.username());
+        user.setPassword(encoder.encode(userDto.password()));
+        user.setVerificationCode(verificationCode);
+        user.setVerified(false);
+        return repo.save(user);
+    }
+
+    public User updateUser(User user){
+        return repo.save(user);
+    }
+
+    public User getUserByEmail(String email) {
+        return repo.findByEmail(email);
+    }
 }
