5b419d436f807e94cf5ee87f9967b50b520baed7
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2025-11-01 17:25:41 +0530

    Configured to generate JWT token

--- chapter-backend/src/main/java/org/varun/chapterbackend/service/JwtService.java
+++ chapter-backend/src/main/java/org/varun/chapterbackend/service/JwtService.java
@@ -1,4 +1,53 @@
 package org.varun.chapterbackend.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+
+@Service
 public class JwtService {
+    @Value("${spring.jwt.expiration_time}")
+    private long expiration;
+
+    private final String secret;
+
+    public JwtService(){
+        this.secret=generateSecret();
+    }
+
+    private String generateSecret(){
+        try{
+            KeyGenerator keyGen=KeyGenerator.getInstance("HmacSHA256");
+            SecretKey secretKey=keyGen.generateKey();
+            return Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        }catch (NoSuchAlgorithmException e){
+            throw new RuntimeException("Error while generating secret: ",e);
+        }
+    }
+
+    public String generateToken(String username){
+        return Jwts.builder()
+                .setClaims(new HashMap<>())
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+expiration))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Key getKey(){
+        byte[] keyBytes= Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
