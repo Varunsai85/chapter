3a9dd6b5166e6945427776bb1f8c80f0af381a66
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2025-11-07 20:32:36 +0530

    Corrected imported class

--- chapter-backend/src/main/java/org/varun/chapterbackend/config/ApplicationConfig.java
+++ chapter-backend/src/main/java/org/varun/chapterbackend/config/ApplicationConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.varun.authrevision.service.UserService;
+import org.varun.chapterbackend.service.UserService;
 
 @Configuration
 @AllArgsConstructor
